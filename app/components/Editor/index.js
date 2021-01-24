ae5b1186ce397dc29a691c45460cf2164dd9fd05
Marco van Vemden <vanvemden@gmail.com>
2021-01-23 17:37:51 -0800

    Add Editor component

--- app/components/Editor/index.js
+++ app/components/Editor/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ControlledEditor from '@monaco-editor/react';
 
+import { EditorWrapper } from './styles';
+
 export function Editor({
   height = 100,
   language = 'javascript',
   theme = 'light',
   value = 'Code goes here',
-  width = 400,
+  width = '100%',
 }) {
   return (
-    <ControlledEditor
-      height={height}
-      language={language}
-      theme={theme}
-      value={value}
-      width={width}
-    />
+    <EditorWrapper>
+      <ControlledEditor
+        height={height}
+        language={language}
+        theme={theme}
+        value={value}
+        width={width}
+      />
+    </EditorWrapper>
   );
 }
 
---
+++ app/components/Editor/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const EditorWrapper = styled.div`
+  margin: 20px;
+  background-color: white;
+  border: 1px solid orange;
+`;
