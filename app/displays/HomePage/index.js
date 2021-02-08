f66a0d1932d8f9eb7361f17942be4efd51921a4d
Marco van Vemden <vanvemden@gmail.com>
2021-02-07 18:23:55 -0800

    Move HomePage to displays folder

---
+++ app/displays/HomePage/index.js
@@ -0,0 +1,18 @@
+/*
+ * HomePage
+ *
+ * Home sweet home
+ *
+ */
+
+import React from 'react';
+
+import Dashboard from '../Dashboard';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
