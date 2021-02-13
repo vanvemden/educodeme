5b42f0fa64cd82e9b47062f53d05f0da2c55d5ad
Marco van Vemden <vanvemden@gmail.com>
2021-02-12 23:51:20 -0800

    Add reusable connection signal indicator component

--- app/containers/WebsocketConnector/components/SendIndicator.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-import { SignalIndicator as BaseSignalIndicator } from '../../../components/Output';
-
-const StyledSignalIndicator = styled(BaseSignalIndicator)``;
-
-export default StyledSignalIndicator;
