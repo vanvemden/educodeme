71b87f0612ce95a348fb24a52abc84ec725ad774
Marco van Vemden <vanvemden@gmail.com>
2021-02-14 12:14:13 -0800

    Make path session id param optional

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <StylesProvider injectFirst>
       <Switch>
-        <Route exact path="/session/:session_id" component={Dashboard} />
+        <Route exact path="/session/:session_id?" component={Dashboard} />
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
