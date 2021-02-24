34f772f8ee4e4c378b2863fbb543593e12afe8ec
Marco van Vemden <vanvemden@gmail.com>
2021-02-23 22:00:17 -0800

    Cleanup, add comments

--- app/containers/WebsocketConnector/actions.js
+++ app/containers/WebsocketConnector/actions.js
@@ -4,42 +4,194 @@ import {
   WEBSOCKET_CONNECTOR_PUBLISH_SESSION_FAILURE,
   WEBSOCKET_CONNECTOR_PUBLISH_SESSION_SUCCESS,
   WEBSOCKET_CONNECTOR_PUBLISH_SESSION,
+  WEBSOCKET_CONNECTOR_PUBLISH_USER_FAILURE,
+  WEBSOCKET_CONNECTOR_PUBLISH_USER_SUCCESS,
+  WEBSOCKET_CONNECTOR_PUBLISH_USER,
   WEBSOCKET_CONNECTOR_SESSION_ACTION_RECEIVED,
   WEBSOCKET_CONNECTOR_SESSION_ACTION_SENT,
-  WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_CONNECTION,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_ACTIONS_FAILURE,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_ACTIONS_SUCCESS,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_ACTIONS,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_FAILURE,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_SUCCESS,
+  WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_FAILURE,
+  WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_SUCCESS,
+  WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS,
   WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION,
   WEBSOCKET_CONNECTOR_TOPIC_ONCHANGE,
   WEBSOCKET_CONNECTOR_UNPUBLISH_SESSION_FAILURE,
   WEBSOCKET_CONNECTOR_UNPUBLISH_SESSION_SUCCESS,
   WEBSOCKET_CONNECTOR_UNPUBLISH_SESSION,
+  WEBSOCKET_CONNECTOR_UNPUBLISH_USER_FAILURE,
+  WEBSOCKET_CONNECTOR_UNPUBLISH_USER_SUCCESS,
+  WEBSOCKET_CONNECTOR_UNPUBLISH_USER,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_ACTIONS_FAILURE,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_ACTIONS_SUCCESS,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_ACTIONS,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_FAILURE,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_SUCCESS,
+  WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_FAILURE,
+  WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_SUCCESS,
+  WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS,
   WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION,
   WEBSOCKET_CONNECTOR_USERNAME_ONCHANGE,
 } from './constants';
 
 /**
- * Subscribe to connection
+ * @description Dispatched by host when starting a livestream session.
+ * @param {Object} Object with params.
+ * @param {string} params.topic - Session topic.
+ * @param {string} params.username - Host username.
+ * @return {Object} Object with action type and payload.
  */
-export function websocketConnectorSubscribeToConnection() {
+export function websocketConnectorPublishSession({ topic, username }) {
   return {
-    type: WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_CONNECTION,
+    payload: { topic, username },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION,
   };
 }
 
 /**
- * Session action has been sent or received over websocket
+ * @description Dispatched when starting a livestream session failed.
+ * @param {Object} Object with params.
+ * @param {Object} params.error - Error object.
+ * @return {Object} Object with action type and payload.
  */
-export function websocketConnectorSessionActionReceived({ action }) {
-  const { id, payload, timestamp, type } = action;
+export function websocketConnectorPublishSessionFailure({ error }) {
+  return {
+    payload: { error },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION_FAILURE,
+  };
+}
+
+/**
+ * @description Dispatched when starting a livestream session was success.
+ * @param {Object} Object with params.
+ * @param {string} params.id - Session id.
+ * @param {timestamp} params.timestamp - Published date/time.
+ * @param {string} params.token - Session token.
+ * @param {string} params.userId = Host session-user id.
+ * @param {username} params.username - Host username
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorPublishSessionSuccess({
+  id,
+  timestamp,
+  token,
+  topic,
+  username,
+}) {
+  return {
+    payload: { id, timestamp, token, topic, username },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION_SUCCESS,
+  };
+}
+
+/**
+ * @description Dispatched to publish a user.
+ * @param {Object} Object with params.
+ * @param {string} params.sessionId - Session Id.
+ * @param {string} params.username - Host username.
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorPublishUser({ sessionId, username }) {
+  return {
+    payload: { sessionId, username },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_USER,
+  };
+}
+
+/**
+ * @description Dispatched when publish a user failed.
+ * @param {Object} Object with params.
+ * @param {Object} params.error - Error object.
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorPublishUserFailure({ error }) {
+  return {
+    payload: { error },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_USER_FAILURE,
+  };
+}
+
+/**
+ * @description Dispatched when publish a user was success.
+ * @param {Object} Object with params.
+ * @param {string} params.id - User id.
+ * @param {string} params.sessionId - Session id.
+ * @param {string} params.token - User token.
+ * @param {username} params.username - Username
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorPublishUserSuccess({
+  id,
+  sessionId,
+  token,
+  username,
+}) {
+  return {
+    payload: { id, sessionId, token, username },
+    type: WEBSOCKET_CONNECTOR_PUBLISH_USER_SUCCESS,
+  };
+}
+
+/**
+ * @description Dispatched to unpublish a user.
+ * @param {Object} Object with params.
+ * @param {string} params.topic - Session topic.
+ * @param {string} params.username - Host username.
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorUnpublishUser({ id, sessionId, token }) {
+  return {
+    payload: { id, sessionId, token },
+    type: WEBSOCKET_CONNECTOR_UNPUBLISH_USER,
+  };
+}
+
+/**
+ * @description Dispatched when unpublish user failed.
+ * @param {Object} Object with params.
+ * @param {Object} params.error - Error object.
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorUnpublishUserFailure({ error }) {
+  return {
+    payload: { error },
+    type: WEBSOCKET_CONNECTOR_UNPUBLISH_USER_FAILURE,
+  };
+}
+
+/**
+ * @description Dispatched when unpublish user was success.
+ * @param {Object} Object with params.
+ * @param {string} params.id - User id.
+ * @param {string} params.sessionId - Session id.
+ * @param {string} params.token - User token.
+ * @param {username} params.username - Username
+ * @return {Object} Object with action type and payload.
+ */
+export function websocketConnectorUnpublishUserSuccess({ id, sessionId }) {
+  return {
+    payload: { id, sessionId },
+    type: WEBSOCKET_CONNECTOR_UNPUBLISH_USER_SUCCESS,
+  };
+}
+
+/**
+ * Session action was received over websocket connection
+ * @param {Object} payload - Action payload.
+ * @param {uuid} .id - Session id.
+ * @param {Object} payload.payload - Payload of action received.
+ * @param {timestamp} payload.timestamp - Publish data/time.
+ * @param {string} payload.type - Action type.
+ */
+export function websocketConnectorSessionActionReceived({
+  id,
+  payload,
+  timestamp,
+  type,
+}) {
   return {
     payload: { id, payload, timestamp, type },
     type: WEBSOCKET_CONNECTOR_SESSION_ACTION_RECEIVED,
@@ -72,30 +224,6 @@ export function websocketConnectorPublishActionSuccess({
   return {
     payload: { id, index, payload, sessionId, type },
     type: WEBSOCKET_CONNECTOR_PUBLISH_ACTION_SUCCESS,
-  };
-}
-
-/**
- * Publish session, failure and success
- */
-export function websocketConnectorPublishSession({ topic, username }) {
-  return {
-    payload: { topic, username },
-    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION,
-  };
-}
-
-export function websocketConnectorPublishSessionFailure({ error }) {
-  return {
-    payload: { error },
-    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION_FAILURE,
-  };
-}
-
-export function websocketConnectorPublishSessionSuccess({ id, token }) {
-  return {
-    payload: { id, token },
-    type: WEBSOCKET_CONNECTOR_PUBLISH_SESSION_SUCCESS,
   };
 }
 
@@ -221,19 +349,48 @@ export function websocketConnectorUnsubscribeSessionActionsSuccess() {
 }
 
 /**
- * Disconnect the host or join session
+ * Subscribe to receive session users, failure and success
  */
-export function websocketConnectorDisconnectHost({ id, username }) {
+export function websocketConnectorSubscribeToSessionUsers({ id }) {
   return {
-    payload: { id, username },
-    type: WEBSOCKET_CONNECTOR_DISCONNECT_HOST,
+    payload: { id },
+    type: WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS,
   };
 }
 
-export function websocketConnectorDisconnectJoin({ id, username }) {
+export function websocketConnectorSubscribeToSessionUsersFailure({ error }) {
   return {
-    payload: { id, username },
-    type: WEBSOCKET_CONNECTOR_DISCONNECT_JOIN,
+    payload: { error },
+    type: WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_FAILURE,
+  };
+}
+
+export function websocketConnectorSubscribeToSessionUsersSuccess() {
+  return {
+    type: WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_SUCCESS,
+  };
+}
+
+/**
+ * Unsubscribe to receive session actions, and handle failure and success
+ */
+export function websocketConnectorUnsubscribeSessionUsers({ id }) {
+  return {
+    payload: { id },
+    type: WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS,
+  };
+}
+
+export function websocketConnectorUnsubscribeSessionUsersFailure({ error }) {
+  return {
+    payload: { error },
+    type: WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_FAILURE,
+  };
+}
+
+export function websocketConnectorUnsubscribeSessionUsersSuccess() {
+  return {
+    type: WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_SUCCESS,
   };
 }
 
