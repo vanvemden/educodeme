157152e289570faf4c71637fc7f4f2c214fa6101
Marco van Vemden <vanvemden@gmail.com>
2021-02-23 22:01:00 -0800

    Cleanup

--- app/containers/WebsocketConnector/constants.js
+++ app/containers/WebsocketConnector/constants.js
@@ -24,8 +24,21 @@ export const WEBSOCKET_CONNECTOR_PUBLISH_ACTION_FAILURE =
 export const WEBSOCKET_CONNECTOR_PUBLISH_ACTION_SUCCESS =
   'app/containers/WebsocketConnector/PUBLISH_ACTION_SUCCESS';
 
-export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_CONNECTION =
-  'app/containers/WebsocketConnector/SUBSCRIBE_TO_CONNECTION';
+export const WEBSOCKET_CONNECTOR_PUBLISH_USER =
+  'app/containers/WebsocketConnector/PUBLISH_USER';
+export const WEBSOCKET_CONNECTOR_PUBLISH_USER_FAILURE =
+  'app/containers/WebsocketConnector/PUBLISH_USER_FAILURE';
+export const WEBSOCKET_CONNECTOR_PUBLISH_USER_SUCCESS =
+  'app/containers/WebsocketConnector/PUBLISH_USER_SUCCESS';
+export const WEBSOCKET_CONNECTOR_PUBLISH_USER_SUCCESS_FROM_WEBSOCKET =
+  'app/containers/WebsocketConnector/PUBLISH_USER_SUCCESS_FROM_WEBSOCKET';
+
+export const WEBSOCKET_CONNECTOR_UNPUBLISH_USER =
+  'app/containers/WebsocketConnector/UNPUBLISH_USER';
+export const WEBSOCKET_CONNECTOR_UNPUBLISH_USER_FAILURE =
+  'app/containers/WebsocketConnector/UNPUBLISH_USER_FAILURE';
+export const WEBSOCKET_CONNECTOR_UNPUBLISH_USER_SUCCESS =
+  'app/containers/WebsocketConnector/UNPUBLISH_USER_SUCCESS';
 
 export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION =
   'app/containers/WebsocketConnector/SUBSCRIBE_TO_SESSION';
@@ -55,6 +68,22 @@ export const WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_ACTIONS_FAILURE =
 export const WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_ACTIONS_SUCCESS =
   'app/containers/WebsocketConnector/UNSUBSCRIBE_SESSION_ACTIONS_SUCCESS';
 
+export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS =
+  'app/containers/WebsocketConnector/SUBSCRIBE_TO_SESSION_USERS';
+export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_FAILURE =
+  'app/containers/WebsocketConnector/SUBSCRIBE_TO_SESSION_USERS_FAILURE';
+export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_SUCCESS =
+  'app/containers/WebsocketConnector/SUBSCRIBE_TO_SESSION_USERS_SUCCESS';
+export const WEBSOCKET_CONNECTOR_SUBSCRIBE_TO_SESSION_USERS_FROM_WEBSOCKET =
+  'app/containers/WebsocketConnector/SUBSCRIBE_TO_SESSION_USERS_FROM_WEBSOCKET';
+
+export const WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS =
+  'app/containers/WebsocketConnector/UNSUBSCRIBE_SESSION_USERS';
+export const WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_FAILURE =
+  'app/containers/WebsocketConnector/UNSUBSCRIBE_SESSION_USERS_FAILURE';
+export const WEBSOCKET_CONNECTOR_UNSUBSCRIBE_SESSION_USERS_SUCCESS =
+  'app/containers/WebsocketConnector/UNSUBSCRIBE_SESSION_USERS_SUCCESS';
+
 export const WEBSOCKET_CONNECTOR_CONNECT =
   'app/containers/WebsocketConnector/CONNECT';
 export const WEBSOCKET_CONNECTOR_CONNECT_FROM_WEBSOCKET =
@@ -65,20 +94,9 @@ export const WEBSOCKET_CONNECTOR_DISCONNECT_FROM_WEBSOCKET =
   'app/containers/WebsocketConnector/DISCONNECT_FROM_WEBSOCKET';
 export const WEBSOCKET_CONNECTOR_RESET =
   'app/containers/WebsocketConnector/RESET';
-export const WEBSOCKET_CONNECTOR_RESET_FROM_WEBSOCKET =
-  'app/containers/WebsocketConnector/RESET_FROM_WEBSOCKET';
-
-export const WEBSOCKET_CONNECTOR_AUTOPILOT_OFF =
-  'app/containers/WebsocketConnector/AUTOPILOT_OFF';
-export const WEBSOCKET_CONNECTOR_AUTOPILOT_ON =
-  'app/containers/WebsocketConnector/AUTOPILOT_ON';
-export const WEBSOCKET_CONNECTOR_AUTOPILOT_SETTINGS =
-  'app/containers/WebsocketConnector/AUTOPILOT_SETTINGS';
 
 // Input field action types
 export const WEBSOCKET_CONNECTOR_USERNAME_ONCHANGE =
   'app/containers/WebsocketConnector/USERNAME_ONCHANGE';
 export const WEBSOCKET_CONNECTOR_TOPIC_ONCHANGE =
   'app/containers/WebsocketConnector/TOPIC_ONCHANGE';
-export const WEBSOCKET_CONNECTOR_TOPIC_ONCHANGE_FROM_WEBSOCKET =
-  'app/containers/WebsocketConnector/TOPIC_ONCHANGE_FROM_WEBSOCKET';
